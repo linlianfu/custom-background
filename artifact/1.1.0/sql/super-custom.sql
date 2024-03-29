a2f2f04ffde5d1756fce2ae1ce78a4f2923a40e0
linlianfu <1241352445@qq.com>
2024-03-29 12:25:34 +0800

    数据库调整

--- artifact/1.1.0/sql/super-custom.sql
+++ artifact/1.1.0/sql/super-custom.sql
@@ -1,13 +1,17 @@
 CREATE TABLE `cb_store` (
   `st_id` varchar(32) NOT NULL COMMENT '店铺 id',
   `st_store_name` varchar(255) DEFAULT NULL COMMENT '店铺名称',
-  `st_shop_id` varchar(32) DEFAULT NULL COMMENT '商户 id',
+  `st_legal_person` char(100) DEFAULT NULL COMMENT '法人',
+  `st_business_license` varchar(255) DEFAULT NULL COMMENT '营业执照',
   `st_registration_time` datetime DEFAULT NULL COMMENT '注册时间',
-  `st_platform_type` tinyint(4) DEFAULT NULL COMMENT '所属平台',
   `st_tort_fraction` int(11) DEFAULT NULL COMMENT '侵权扣分',
   `st_serious_trot_count` tinyint(4) DEFAULT NULL COMMENT '严重侵权次数',
+  `st_status` tinyint(4) DEFAULT NULL COMMENT '店铺状态 | 1：运营  2、退出运营',
+  `st_shop_id` varchar(32) DEFAULT NULL COMMENT '商户 id',
+  `st_exit_time` date DEFAULT NULL COMMENT '退出时间',
+  `st_platform_type` tinyint(4) DEFAULT NULL COMMENT '所属平台',
   `st_create_time` datetime NOT NULL COMMENT '创建时间',
-  `st_cerate_id` varchar(32) NOT NULL COMMENT '创建人',
+  `st_create_id` varchar(32) NOT NULL COMMENT '创建人',
   PRIMARY KEY (`st_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='店铺管理';
 
@@ -18,18 +22,21 @@ CREATE TABLE `cb_store_theme` (
   `sth_up_time` datetime DEFAULT NULL COMMENT '上架时间',
   `sth_product_count` tinyint(4) DEFAULT NULL COMMENT '上架产品数量',
   `sth_tort` tinyint(1) DEFAULT NULL COMMENT '是否侵权',
-  `sth_tort_type` tinyint(4) DEFAULT NULL COMMENT '侵权类型',
+  `sth_tort_type` tinyint(4) DEFAULT NULL COMMENT '侵权类型 | 1: 知产平台治理-一般侵权 2:知识产权-一般侵权 3:资金冻结 4:严重侵权',
   `sth_tort_time` datetime DEFAULT NULL COMMENT '侵权时间',
-  `sth_create_id` char(10) DEFAULT NULL COMMENT '创建人',
+  `sth_intellectual_property_name` varchar(255) DEFAULT NULL COMMENT '知识产权名称',
+  `sth_remark` varchar(255) DEFAULT NULL COMMENT '备注',
+  `sth_create_id` varchar(32) DEFAULT NULL,
   `sth_create_time` datetime DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='店铺主题管理';
+
 CREATE TABLE `cb_theme` (
   `th_id` varchar(32) NOT NULL COMMENT '主题id',
   `th_name` varchar(255) DEFAULT NULL COMMENT '主题名称',
   `th_keyword` varchar(255) DEFAULT NULL COMMENT '关键词',
   `th_category_id` varchar(32) DEFAULT NULL COMMENT '主题分类',
-  `th_tort_type` tinyint(4) DEFAULT NULL COMMENT '风险等级 | 1、常规主题  2、一般侵权 3、资金冻结 4、严重侵权',
+  `th_risk_type` tinyint(4) DEFAULT NULL COMMENT '风险等级 | 1、常规主题  2、一般侵权 3、资金冻结 4、严重侵权',
   `th_flow` tinyint(4) DEFAULT NULL COMMENT '流量等级 | 1、普通 2、爆款',
   `th_remark` varchar(255) DEFAULT NULL COMMENT '备注',
   `th_create_id` varchar(32) DEFAULT NULL COMMENT '创建人',
@@ -41,12 +48,10 @@ CREATE TABLE `cb_theme` (
 -- 03.28 调整字段类型
 ALTER TABLE sys_user MODIFY user_id varchar(32);
 ALTER TABLE sys_role MODIFY role_id varchar(32);
-ALTER TABLE cb_store MODIFY st_id varchar(32);
 ALTER TABLE sys_users_roles MODIFY user_id varchar(32);
 ALTER TABLE sys_users_roles MODIFY role_id varchar(32);
 ALTER TABLE sys_users_jobs MODIFY user_id varchar(32);
 ALTER TABLE sys_roles_menus MODIFY role_id varchar(32);
-ALTER TABLE cb_store_theme MODIFY sth_create_id varchar(32);
 
 update sys_user set user_id = '2d0e5908df3811e8a4ec2afb6e36c22c' where user_id = 1;
 update sys_role set role_id = '53af3b61e02711e8a4ec2afb6e36c22c' where role_id = 1;
